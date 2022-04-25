dd3539f695d0fad2745fd8fe74fd9d62fa5848c9
gahr <gahr>
2022-04-25 13:05:25 +0000

    Use cmdline to parse command-line arguments

--- src/run.tcl
+++ src/run.tcl
@@ -1,7 +1,8 @@
 ##
 # Run the server
 
-::server::fork ;# doesn't return if forked
+set args $::argv
 ::server::parse_args
+::server::fork $args; # doesn't return if forked
 ::server::serve
 vwait _forever
